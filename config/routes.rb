793da22d8bb213a17b82a6e77b56ee915ad15b92
tran.thanh.tam <FRAMGIA\tran.thanh.tam@b121031-pc.framgia.com>
2019-05-29 13:03:47 +0700

    Login logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
 
+  devise_for :admins, controllers: {sessions: "admins/sessions", passwords: "admins/passwords"}
+
+  devise_scope :admin do
+    get "/admins/sign_out" => "devise/sessions#destroy"
+  end
+
   namespace :admins do
     resources :home, only: :index
   end
