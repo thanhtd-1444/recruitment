a318b16199ca8ef25558e22075c191ecfacd9f3f
tamtt-1031 <tran.thanh.tam@sun-asterisk.com>
2019-06-19 19:25:44 +0700

    Fix locale

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,5 @@
 require_relative 'boot'
 require 'rails/all'
-require 'dotenv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,7 +11,6 @@ module Recruitment
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.autoload_paths += %W(#{config.root}/vendor/assets/owlcarousel)
-    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
