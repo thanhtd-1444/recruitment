fdd00aa45a5cba7a34b252b62891fab39bf81c50
thaithanhdhv <thaithanhdhv.vn@gmail.com>
2019-07-23 15:32:30 +0700

    Add validate

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,8 @@
 class Job < ApplicationRecord
-  validates :title, :quantity, :place, :salary, :start_date, :end_date, presence: true
+  validates :title, :quantity, :place,
+            :content, :working_hours, :allowance, :salary_raise,
+            :holiday, :vacation, :social_insurance, :welfare, :documents,
+            :salary, :start_date, :end_date, presence: true
 
   scope :newest, ->{order :end_date}
   scope :available, -> {where("? BETWEEN start_date AND end_date", Date.current)}
